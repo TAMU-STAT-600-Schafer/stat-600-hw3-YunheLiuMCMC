a2eefebc94002fc906bb7de9422e3f1333282f5b
Yunhe Liu <yunhe.liu@tamu.edu>
2024-09-27 16:58:53 -0500

    Added column for an intercept to X and Xt

--- Letter_example.R
+++ Letter_example.R
@@ -13,6 +13,8 @@ Yt <- letter_test[, 1]
 Xt <- as.matrix(letter_test[, -1])
 
 # [ToDo] Make sure to add column for an intercept to X and Xt
+X <- cbind(1, X)
+Xt <- cbind(1, Xt)
 
 # Source the LR function
 source("FunctionsLR.R")
